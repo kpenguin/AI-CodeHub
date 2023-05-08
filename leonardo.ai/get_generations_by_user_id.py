71214d68dd961ad274b11c45ee608ea1f41c4ef7
kpenguin <kpenguin@users.noreply.github.com>
2023-05-08 16:37:42 -0600

    Update files...

--- leonardo.ai/get_generations_by_user_id.py
+++ leonardo.ai/get_generations_by_user_id.py
@@ -2,33 +2,39 @@
 ## GET https://cloud.leonardo.ai/api/rest/v1/generations/user/{userId}
 ## This endpoint returns all generations by a specific user
 
+###############################################################################
+
+## Get program path and name
+from pathlib import Path
+myfile = Path(__file__).stem
+
 ## Read in configuration file
 import yaml
 
 def read_yaml(file_path):
     with open(file_path, "r") as f:
         return yaml.safe_load(f)
-    
-myprivate = read_yaml("myprivate.config")
 
-# print(myprivate)
-# print(myprivate['LEONARDOAI'])
-# print(myprivate['LEONARDOAI']['APIKey'])
+myPrivateConfig = "../myprivate.config"
+myprivate = read_yaml(myPrivateConfig)
+
 
 ## Validation configuration file
 import pytest
 
 def test_validation_yaml():
     with pytest.raises(FileNotFoundError):
-        read_yaml(file_path="source/data/non_existing_file.yaml")
+        read_yaml(file_path=myPrivateConfig)
 
     with pytest.raises(yaml.scanner.ScannerError):
         # only show the first error
-        read_yaml(file_path="source/data/sample_invalid.yaml")
+        read_yaml(file_path=myPrivateConfig)
 
     with pytest.raises(yaml.parser.ParserError):
         # only show the first error
-        read_yaml(file_path="source/data/sample_invalid.json")
+        read_yaml(file_path=myPrivateConfig)
+
+###############################################################################
 
 
 ## Main
@@ -46,24 +52,24 @@ headers = {
 }
 
 response = requests.get(url, headers=headers)
-
 print(response.text)
 
-with open("studentWithoutPrettyPrint.json", "w") as write_file:
-    json.dump(student, write_file)
-print("Done writing JSON data into file without Pretty Printing it")
+import json
+mydata = response.text
 
+with open(myfile + "-raw.json", "w") as write_file:
+    json.dump(mydata, write_file)
 
-with open("studentWithPrettyPrint2.json", "w") as write_file:
-    json.dump(student, write_file, indent=0)
-print("Done writing PrettyPrinted JSON data into file with indent=0")
+with open(myfile + "-pretty.json", "w") as write_file:
+    json.dump(mydata, write_file, indent=4)
 
-with open("studentWithPrettyPrint3.json", "w") as write_file:
-    json.dump(student, write_file, indent=4, sort_keys=True)
-print("Done writing Sorted and PrettyPrinted JSON data into file")
+with open(myfile + "-sorted.json", "w") as write_file:
+    json.dump(mydata, write_file, indent=4, sort_keys=True)
 
+""" 
 import pandas as pd
 d = pd.read_json(response.text)
 df = pd.json_normalize(d)
 print(df)
 # df.to_csv('gen_by_userid.csv', encoding='utf-8', index=False)
+ """
--- leonardo.ai/get_user_information.py
+++ leonardo.ai/get_user_information.py
@@ -2,33 +2,39 @@
 ## GET https://cloud.leonardo.ai/api/rest/v1/me
 ## This endpoint will return your user information, including your user ID.
 
+###############################################################################
+
+## Get program path and name
+from pathlib import Path
+myfile = Path(__file__).stem
+
 ## Read in configuration file
 import yaml
 
 def read_yaml(file_path):
     with open(file_path, "r") as f:
         return yaml.safe_load(f)
-    
-myprivate = read_yaml("myprivate.config")
-# print(myprivate)
-# print(myprivate['LEONARDOAI'])
-# print(myprivate['LEONARDOAI']['APIKey'])
+
+myPrivateConfig = "../myprivate.config"
+myprivate = read_yaml(myPrivateConfig)
+
 
 ## Validation configuration file
 import pytest
 
 def test_validation_yaml():
     with pytest.raises(FileNotFoundError):
-        read_yaml(file_path="source/data/non_existing_file.yaml")
+        read_yaml(file_path=myPrivateConfig)
 
     with pytest.raises(yaml.scanner.ScannerError):
         # only show the first error
-        read_yaml(file_path="source/data/sample_invalid.yaml")
+        read_yaml(file_path=myPrivateConfig)
 
     with pytest.raises(yaml.parser.ParserError):
         # only show the first error
-        read_yaml(file_path="source/data/sample_invalid.json")
+        read_yaml(file_path=myPrivateConfig)
 
+###############################################################################
 
 ## Main
 import requests
@@ -41,5 +47,16 @@ headers = {
 }
 
 response = requests.get(url, headers=headers)
-
 print(response.text)
+
+import json
+mydata = response.text
+
+with open(myfile + "-raw.json", "w") as write_file:
+    json.dump(mydata, write_file)
+
+with open(myfile + "-pretty.json", "w") as write_file:
+    json.dump(mydata, write_file, indent=4)
+
+with open(myfile + "-sorted.json", "w") as write_file:
+    json.dump(mydata, write_file, indent=4, sort_keys=True)
